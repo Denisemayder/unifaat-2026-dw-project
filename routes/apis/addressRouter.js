0b252e1541bacdc794795ba21564944e4a93ce2b
Denise Maider <denisemaider@gmail.com>
2026-04-30 23:48:10 -0300

    construção de uma API REST tf09

--- routes/apis/addressRouter.js
+++ routes/apis/addressRouter.js
@@ -1,15 +1,24 @@
 
+import  CreateAddressController  from "../../app/Controllers/AddressApi/CreateAddressController.js";
+import  GetAddressController  from "../../app/Controllers/AddressApi/GetAddaressController.js";
+import  ListAddressController  from "../../app/Controllers/AddressApi/ListAddressController.js";
+import  UpdateAddressController  from "../../app/Controllers/AddressApi/UpdateAddresscontroller.js";
+import  DeleteAddressController  from "../../app/Controllers/AddressApi/DeleteAddressController.js";
+import { Router } from "express"; 
 
-import { Router } from "express";
 
 export default (() => {
     const router = Router();
 
-    /** 
-     * TF 09
-     * 
-     * Insira as 5 rotas aqui
-     */
+      router.get('/', ListAddressController);
+    
+        router.post('/', CreateAddressController );
+    
+        router.post('/', CreateAddressController);
+    
+        router.put('/:id', UpdateAddressController);
+    
+        router.delete('/:id', DeleteAddressController)
 
     return router;
 })();
